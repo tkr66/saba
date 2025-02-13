4584501002dd40dc997d26b1b9a5463af8ba5f11
tkr66 <plumium@outlook.com>
2025-02-13 09:53:15 +0900

    Parse states for Data, TagOpen, EndTagOpen and TagName

--- saba_core/src/renderer/html/token.rs
+++ saba_core/src/renderer/html/token.rs
@@ -26,6 +26,46 @@ impl HtmlTokenizer {
     fn is_eof(&self) -> bool {
         self.pos > self.input.len()
     }
+
+    /// Creates a start tag if start_tag_token is true.
+    /// otherwise create a end tag.
+    fn create_tag(&mut self, start_tag_token: bool) {
+        if start_tag_token {
+            self.latest_token = Some(HtmlToken::StartTag {
+                tag: String::new(),
+                self_closing: false,
+                attributes: Vec::new(),
+            })
+        } else {
+            self.latest_token = Some(HtmlToken::EndTag { tag: String::new() })
+        }
+    }
+
+    fn reconsume_input(&mut self) -> char {
+        self.reconsume = false;
+        self.input[self.pos - 1]
+    }
+
+    fn take_latest_token(&mut self) -> Option<HtmlToken> {
+        assert!(self.latest_token.is_some());
+        self.latest_token.take()
+    }
+
+    fn append_tag_name(&mut self, c: char) {
+        assert!(self.latest_token.is_some());
+
+        if let Some(t) = self.latest_token.as_mut() {
+            match t {
+                HtmlToken::StartTag {
+                    tag,
+                    self_closing: _,
+                    attributes: _,
+                } => tag.push(c),
+                HtmlToken::EndTag { tag } => tag.push(c),
+                _ => panic!("`latest_token` should be either StartTag or EndTag"),
+            }
+        }
+    }
 }
 
 pub enum HtmlToken {
@@ -83,6 +123,14 @@ pub enum State {
     TemporaryBuffer,
 }
 
+impl HtmlTokenizer {
+    fn consume_next_input(&mut self) -> char {
+        let c = self.input[self.pos];
+        self.pos += 1;
+        c
+    }
+}
+
 impl Iterator for HtmlTokenizer {
     type Item = HtmlToken;
 
@@ -92,11 +140,67 @@ impl Iterator for HtmlTokenizer {
         }
 
         loop {
+            let c = match self.reconsume {
+                true => self.reconsume_input(),
+                false => self.consume_next_input(),
+            };
             match self.state {
-                State::Data => todo!(),
-                State::TagOpen => todo!(),
-                State::EndTagOpen => todo!(),
-                State::TagName => todo!(),
+                State::Data => {
+                    if c == '<' {
+                        self.state = State::TagOpen;
+                        continue;
+                    }
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+                    return Some(HtmlToken::Char(c));
+                }
+                State::TagOpen => {
+                    if c == '/' {
+                        self.state = State::EndTagOpen;
+                        continue;
+                    }
+                    if c.is_ascii_alphabetic() {
+                        self.reconsume = true;
+                        self.state = State::TagName;
+                        self.create_tag(true);
+                        continue;
+                    }
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+                    self.reconsume = true;
+                    self.state = State::Data;
+                }
+                State::EndTagOpen => {
+                    if self.is_eof() {
+                        return Some(HtmlToken::Eof);
+                    }
+                    if c.is_ascii_alphabetic() {
+                        self.reconsume = true;
+                        self.state = State::TagName;
+                        self.create_tag(false);
+                        continue;
+                    }
+                }
+                State::TagName => {
+                    if c.is_whitespace() {
+                        self.state = State::BeforeAttributeName;
+                        continue;
+                    }
+                    if c == '/' {
+                        self.state = State::SelfClosingStartTag;
+                        continue;
+                    }
+                    if c == '>' {
+                        self.state = State::Data;
+                        return self.take_latest_token();
+                    }
+                    if c.is_ascii_uppercase() {
+                        self.append_tag_name(c.to_ascii_lowercase());
+                        continue;
+                    }
+                }
                 State::BeforeAttributeName => todo!(),
                 State::AttributeName => todo!(),
                 State::AfterAttributeName => todo!(),
